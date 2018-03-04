f11e4bd2757e78cb1801a84915ec254722563006
CaptTeemo <siarfeat@gmail.com>
2018-03-04 18:56:30 +0100

    ÜBersetzt auf Deutsch

--- gui_cookbook.py
+++ gui_cookbook.py
@@ -5,31 +5,32 @@ from tkinter import ttk
 #Erzeugen eines Fensters
 win = tk.Tk()
 
-#Hinzufügen eines Titels
+#Titel des Fensters
 win.title("Python GUI")
 
-#Hinzufügen eines Labels das modifiziert werden kann
+#Label 
 a_label = ttk.Label(win, text= "Enter a Name:")
 a_label.grid(column =0, row=0)
 
-#Button Click event function
-def click_me():
-	action.configure(text="Hello " + name.get() + ' ' + number.get())
+#2tes Label
+ttk.Label(win, text="Choose a Number: ").grid(column=1, row=0)
+number = tk.StringVar()
 
-#Adding a Textbox
+#Textbox
 name = tk.StringVar()
 name_entered = ttk.Entry(win, width=12, textvariable=name)
 name_entered.grid(column= 0, row=1)
 
-#Hinzufügen eines Buttons
+#Button (param: disabled)
 action=ttk.Button(win,text= "Click Me!", command=click_me)
 action.grid(column =2, row =1)
 #action.config(state='disabled') #Disablen eines Buttons
 
-ttk.Label(win, text="Choose a Number: ").grid(column=1, row=0)
-number = tk.StringVar()
+#Event-Funktion für den Button
+def click_me():
+	action.configure(text="Hello " + name.get() + ' ' + number.get())
 
-#Read-only Combobox
+#Combobox (param:Read-only + Focus auf Eingabe)
 number_chosen= ttk.Combobox(win, width=12, textvariable= number, state='readonly')
 number_chosen['values'] = (1,2,42,100)
 number_chosen.grid(column=1, row=1)
@@ -38,6 +39,6 @@ number_chosen.current(0)
 name_entered.focus() #Focus auf das Name Feld mit Eingabe
 
 #=================
-#Start GUI
+#Start der GUI
 win.mainloop()
 #=================
